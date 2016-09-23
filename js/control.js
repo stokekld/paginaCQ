176eed56165b9c6b0e43d7dfe9865181559db06d
Jesus <jesus@trantor>
2016-09-23 11:36:22 -0500

    modificaciones para cargar contenido mediante ajax

---
+++ js/control.js
@@ -0,0 +1,32 @@
+page.base('/caabqys');
+
+page('/', carga); //principal
+page();
+
+function carga(ctx){
+
+    pagina = ctx.hash;
+    ruta = "./content/" + pagina + ".html";
+
+    if (pagina == "")
+        ruta = "./content/principal.html";
+
+    $("#content").hide().load(ruta).fadeIn("500");
+
+}
+
+var dropdown = $(".dropdown");
+var modulo = $(".modulo");
+
+dropdown.mouseover(function(){
+    $(this).addClass("open");
+}).mouseout(function(){
+    dropdown.removeClass("open");
+});
+
+$().ready(function() {
+	            $('.kwicks').kwicks({
+	                maxSize : 250,
+	                behavior: 'menu'
+	            });
+	        });
